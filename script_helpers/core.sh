7c206fe2020e70c30069496ad27e0f9c61367ee3
Michael Campagnaro <mikecampo@gmail.com>
2021-02-10 19:44:00 -0500

    Enable direnv bash hook if program is found

--- script_helpers/core.sh
+++ script_helpers/core.sh
@@ -11,3 +11,13 @@ abort() {
   exit 1
 }
 
+# Use this by setting a trap in your script, like so:
+# `trap at_exit EXIT`
+at_exit() {
+    ret=$?
+    if [[ $ret -gt 0 ]]; then
+      error "The script failed with error $ret.\n"
+    fi
+    exit "$ret"
+}
+
